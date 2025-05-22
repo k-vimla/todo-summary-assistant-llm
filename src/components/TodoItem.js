70565bea1a7623959fff9ae8311b3f724669283a
k-vimla <vimlakushwaha56@gmail.com>
2025-05-22 22:52:38 +0530

    till now

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './TodoItem.css';
 const TodoItem = ({todo}) =>{
     return (
         <div className='todoItem'>
             <h1>{todo.message}</h1>
-            <p>{todo.id}</p>
+           
         </div>
     )
 }
