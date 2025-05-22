df2c73dde695d10fdba36ffa73140051e1938fc2
k-vimla <vimlakushwaha56@gmail.com>
2025-05-22 20:46:15 +0530

    till todos and resolving the error

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 //import logo from './logo.svg';
 import { useState, useEffect } from 'react';
+import Todos from "./components/Todos";
 import axios from 'axios';
 import './App.css';
 
 function App() {
   const [todos, setTodos] = useState(null);
-  useEffect( async () =>{
-    const res = await axios.get('http://localhost:5000');
-    console.log(res);
-  },[])
+  
+  useEffect(() => {
+    const getTodos = async() => {
+      const res= await axios.get("http://localhost:5000");
+      //console.log(res.data)
+      setTodos(res.data)
 
+    
+    }
+    getTodos();
+  },[])
 
 
 
